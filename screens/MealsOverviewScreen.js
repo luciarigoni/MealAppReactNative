1260e7660600056bab871c0eaf910a9d665f488e
Lucia Rigoni Freire <luciarigoni@live.com>
2023-07-27 11:41:43 -0300

    Navigation between screens and displaying meals

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+import { useRoute } from "@react-navigation/native";
+
+function MealsOverviewScreen({ route }) {
+    //const route = useRoute();
+    const catId = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >=0;
+    });
+
+    function renderMealItem(itemData) {
+        return (
+            <MealItem title={itemData.item.title}/>
+        )
+    }
+
+    return(
+        <View style={styles.container}>
+            <FlatList data={displayedMeals} keyExtractor={(item)=> item.id} renderItem={renderMealItem}/>
+        </View>
+    )
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16
+    }
+});
