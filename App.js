c685e6f363c4b17c1627fd5669c4df79d9a98f4e
Lucia Rigoni Freire <luciarigoni@live.com>
2023-07-27 09:57:04 -0300

    Create categories screen and grid

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='light'/>
+      <CategoriesScreen />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,17 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTitle from '../components/CategoryGridTitle';
+
+function renderCategoryItem(itemData) {
+    return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color}/>;
+}
+
+function CategoriesScreen() {
+    return <FlatList data={CATEGORIES} 
+    keyExtractor={(item) => item.id} 
+    renderItem={renderCategoryItem}
+    numColumns={2} 
+    />
+}
+
+export default CategoriesScreen;
