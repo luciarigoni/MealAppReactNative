c31201b03765e2e54b5c7963be91d048066255aa
Lucia Rigoni Freire <luciarigoni@live.com>
2023-07-27 14:42:08 -0300

    files modified

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,10 @@
+import { useLayoutEffect } from "react";
 import { View, FlatList, StyleSheet } from "react-native";
-import { MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
-import { useRoute } from "@react-navigation/native";
 
-function MealsOverviewScreen({ route }) {
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route, navigation }) {
     //const route = useRoute();
     const catId = route.params.categoryId;
 
@@ -11,9 +12,26 @@ function MealsOverviewScreen({ route }) {
         return mealItem.categoryIds.indexOf(catId) >=0;
     });
 
+    useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+    navigation.setOptions({
+        title: categoryTitle
+    });
+    }, [catId, navigation]);
+
     function renderMealItem(itemData) {
+        const item = itemData.item;
+
+        const mealItemProps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration
+        }
         return (
-            <MealItem title={itemData.item.title}/>
+            <MealItem {...mealItemProps}/>
         )
     }
 
