b91b2684c6e36d5da1a7083996e363ce924ea824
Lucia Rigoni Freire <luciarigoni@live.com>
2023-08-01 11:15:19 -0300

    Redux created

---
+++ store/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+{/* Aqui seria a parte que você usaria Redux. Como usei o context, não precisarei atualizar. */}
+
+const store = configureStore({
+    reducer: {}
+});
